357ac2b75f6f7380ed66c1f66596d28967d3998b
MadyX001 <65794252+madian-zaragoza@users.noreply.github.com>
2025-05-23 21:21:33 -0600

    Delete Cerrar.php

--- Cerrar.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();    // Limpia todas las variables de sesión
-session_destroy();  // Destruye la sesión
-
-// Redirige de vuelta al catálogo
-header("Location: Imprenta Cisneros.php"); // Cambia a tu archivo del catálogo si tiene otro nombre
-exit();
